1866fd895e2303221302851ab25e6170053b8db2
Santiago Arcos <santiago.arcos@lyra-network.com>
2019-04-01 18:04:49 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const fetch = require('node-fetch')
+const alert = require('alert-node')
+const { join } = require('path')
+const { createWriteStream, mkdirSync, accessSync} = require('fs')
+const DIR = join(__dirname, 'sitter/making-of')
+const BASE_URL = 'http://helenagonzalezdop.com/sitter/img'
+const firstPhoto = 1
+const lastPhoto = 221
+
+const createFolder = dir => {
+  try {
+    accessSync(dir) // if dir doesn't exist will fail
+  } catch (error) {
+    if (error && error.code === 'ENOENT') {
+      mkdirSync(dir, {recursive: true})
+    }
+  }
+}
+
+const getPhotoName = photoNum => `${photoNum}_g.jpg`
+
+const downloadFile = async (url, absolutePath) => {
+  const res = await fetch(url).catch(e => { throw e })
+  const dest = createWriteStream(absolutePath)
+
+  return new Promise((resolve, reject) => {
+    res.body
+      .pipe(dest)
+      .on('error', err => reject(err))
+      .on('finish', () => resolve())
+  })
+}
+
+const tenCalculator = (val) => {
+	let valSplitter = val / 50
+	let valFloor = Math.floor(valSplitter)
+	let valCeil = Math.ceil(valSplitter)
+	if (valFloor === valCeil) return val
+}
+
+const downloader = async () => {
+  try {
+    createFolder(DIR)
+  } catch (error) {
+    console.error(error)
+  }
+
+  for (let i = firstPhoto; i <= lastPhoto; i++) {
+    const photoName = getPhotoName(i)
+    const status = tenCalculator(i)
+    if (i === 1) alert(`Starting download`)
+    if (status) {
+        console.log(`Downloaded ${i} of ${lastPhoto} photos`)
+        alert(`Downloaded ${i} of ${lastPhoto} photos`)
+    }
+
+    await downloadFile(`${BASE_URL}/${photoName}`, join(DIR, photoName))
+      .catch(console.error)
+  }
+  console.log('Download complete');
+  alert('Download complete');
+}
+
+downloader()
